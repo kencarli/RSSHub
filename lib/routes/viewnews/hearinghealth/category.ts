bdc9e7ea325e0447df07ec2af98dd44fd6ff904b
kencarli <lizhim09@163.com>
2025-10-22 16:51:57 +0800

    Update category.ts

--- lib/routes/viewnews/hearinghealth/category.ts
+++ lib/routes/viewnews/hearinghealth/category.ts
@@ -8,7 +8,7 @@ import timezone from '@/utils/timezone';
 export const route: Route = {
     path: '/hearinghealth/category/:listId?',
     categories: ['hearingnews'],
-    example: '/hearinghealth/category/1/',
+    example: '/hearinghealth/category',
     parameters: { listId: '活动分类，见下表，默认为 `1`' },
     features: {
         requireConfig: false,
@@ -34,12 +34,12 @@ async function handler(ctx) {
     const { data: response } = await got(url);
     const $ = load(response);
 
-    const list = $(listId === '3' ? '#sticky > div > div > div > div > div.x-col.e10758-e9.m8au-m.m8au-n > div > div > a')
+    const list = $(listId === '1' ? '#sticky > div > div > div > div > div > div > div > a')
         .toArray()
         .map((item) => {
             item = $(item);
             return {
-                title: item.find('> article > div.x-text.x-text-headline.e10758-e16.m8au-x.m8au-6 > div > div > h2').text(),
+                title: item.find('> article > div > div > div > h2').text(),
                 link: item.attr('href').startsWith('http') ? item.attr('href') : `${baseUrl}${item.attr('href')}`,
             };
         })
@@ -54,7 +54,7 @@ async function handler(ctx) {
                 //$('.pageLink, .alert, p[style="margin:15px;"]').remove();
 
                 item.description = $('.article').html();
-                item.pubDate = timezone(parseDate($('.article > div.x-section.e10756-e5.m8as-0.m8as-3 > div > div > div > div > div > div.x-col.e10756-e9.m8as-l.m8as-o > div.x-text.x-content.e10756-e12.m8as-s.m8as-t').text().replace('时间:', ''), 'YYYY-MM-DD HH:mm:ss'), +8);
+                item.pubDate = timezone(parseDate($('.article > div > div > div > div > div > div > div > div').text().replace('时间:', ''), 'YYYY-MM-DD HH:mm:ss'), +8);
 
                 return item;
             })
